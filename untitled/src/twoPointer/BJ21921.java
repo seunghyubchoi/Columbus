189fa39ee21053a58bdfe01c8d94477c2c29deff
seunghyubchoi <108142061+seunghyubchoi@users.noreply.github.com>
2024-08-28 19:57:07 +0900

    슬라이딩 윈도우

--- untitled/src/twoPointer/BJ21921.java
+++ untitled/src/twoPointer/BJ21921.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class BJ21921 {
+public class BJ21921 { // slidingWindow
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
