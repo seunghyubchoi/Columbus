e7ab5af3f6e542f3ab636d2d061bb679c942e40e
seunghyubchoi <108142061+seunghyubchoi@users.noreply.github.com>
2024-09-17 13:26:42 +0900

    재귀 기초문제

---
+++ untitled/src/BJ15649.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ15649 {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        visited = new boolean[N];
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == M) {
+            for(int i : arr) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            for (int i = 0; i < N; i++) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    arr[depth] = i + 1;
+                    dfs(depth + 1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
+}
